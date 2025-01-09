3e735c5827d5bad1bab147a4ef68597cde42a648
Ram <153852265+RamChandra1528@users.noreply.github.com>
2025-01-10 02:36:27 +0530

    new

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -26,7 +26,7 @@ function Login() {
             return handleError('Email and password are required');
         }
         try {
-            const url = `https://gaming-community-pi.vercel.app/auth/login`; // Use HTTP if HTTPS is not set up
+            const url = `https://gaming-community-bkd.vercel.app/auth/login`; // Use HTTP if HTTPS is not set up
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -30,7 +30,7 @@ function Signup() {
             return handleError('Name, email, and password are required');
         }
         try {
-            const url = `https://gaming-community-pi.vercel.app/auth/signup`; 
+            const url = `https://gaming-community-bkd.vercel.app/auth/signup`; 
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
